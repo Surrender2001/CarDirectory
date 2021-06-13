38fb9bd7ffc41e5c4b55b9f31f19d6adeabfb084
Sergey <shesergey2001@gmail.com>
2021-06-13 14:10:10 +1000

    ыывф

--- CarDirectory/Car.cs
+++ CarDirectory/Car.cs
@@ -21,14 +21,6 @@ namespace CarDirectory
         public int Start { get; set; }
         public string End { get; set; }
 
-        public DoubleLinkedListNode<object> DoubleLinkedListNode
-        {
-            get => default;
-            set
-            {
-            }
-        }
-
         public bool Equals(Car other)
         {
             if (other == null) return false;
